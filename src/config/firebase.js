3aa363e9d4b8bd75523dc32689f64fe6025bcd6c
Divyanshu Srivastava <87711681+varundxd@users.noreply.github.com>
2024-01-25 23:08:38 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,27 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import {
-//   FacebookAuthProvider,
-//   getAuth,
-//   GoogleAuthProvider,
-// } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBYU9peK55CDnoK1kyrmlmsXNpt8ONfhvY",
-//   authDomain: "streamify-64212.firebaseapp.com",
-//   projectId: "streamify-64212",
-//   storageBucket: "streamify-64212.appspot.com",
-//   messagingSenderId: "279550295355",
-//   appId: "1:279550295355:web:770cb1c6aa68d0c56d8b1e",
-//   measurementId: "G-EL3F3H56ER"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth();
-// export const db = getFirestore(app);
-// export const googleProvider = new GoogleAuthProvider();
-// export const facebookProvider = new FacebookAuthProvider();
-
 import { initializeApp } from "firebase/app";
 import {
   GithubAuthProvider,  // Import GitHubAuthProvider
@@ -31,17 +7,17 @@ import {
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYU9peK55CDnoK1kyrmlmsXNpt8ONfhvY",
-  authDomain: "streamify-64212.firebaseapp.com",
-  projectId: "streamify-64212",
-  storageBucket: "streamify-64212.appspot.com",
-  messagingSenderId: "279550295355",
-  appId: "1:279550295355:web:770cb1c6aa68d0c56d8b1e",
-  measurementId: "G-EL3F3H56ER"
+  apiKey: "your_firebase_api_key",
+  authDomain: "add_firebase_authDomain",
+  projectId: "add_your_project_id",
+  storageBucket: "add_your_storagebucket_value",
+  messagingSenderId: "add_messagingSenderId",
+  appId: "add_your_appId",
+  measurementId: "add_your_measurement_Id"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-export const githubProvider = new GithubAuthProvider();  // Use GithubAuthProvider instead of FacebookAuthProvider
+export const githubProvider = new GithubAuthProvider();  
